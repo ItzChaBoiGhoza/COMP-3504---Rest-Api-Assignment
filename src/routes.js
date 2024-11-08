a1d83fdb00351e79054a5abdab2132fbd2a72efa
ItzChaBoiGhoza <cguy925@gmail.com>
2024-11-08 00:51:25 -0700

    fixed the query param issue, and added try/catch for some routes

--- src/routes.js
+++ src/routes.js
@@ -7,28 +7,31 @@ module.exports.register = (app, database) => {
     });
 
     app.get('/api/items', async (req, res) => {
-        let query;
-        query = database.query('SELECT id, item_name FROM items');
-
-        const records = await query;
-
-        res.status(200).send(JSON.stringify(records)).end();
-    });
-
-    app.get('/api/items', async (req, res) => {
         const item_name = req.query.item_name;
 
-        const records = await database.query('SELECT * FROM items WHERE item_name = ?', [item_name]);
+       try {
+        let records;
 
+        if(item_name) {
+            records = await database.query('SELECT * FROM items WHERE item_name = ?', [item_name]);
+        } else {
+            records = await database.query('SELECT id, item_name FROM items');
+        }
         res.status(200).send(JSON.stringify(records)).end();
-    })
+       } catch (e) {
+        res.status(500).send('Error occured while trying to retrieve items from database');
+       }
+    });
 
     app.get('/api/items/:id', async (req, res) => {
         const id = req.params.id;
 
-        const records = await database.query('SELECT * FROM items WHERE id = ?', [id]);
-
-        res.status(200).send(JSON.stringify(records)).end();
+        try {
+            const records = await database.query('SELECT * FROM items WHERE id = ?', [id]);
+            res.status(200).send(JSON.stringify(records)).end();
+        } catch (e) {
+            res.status(500).send('Error occured while trying to retrieve items from database');
+        }
     })
 
     app.get('/api/suppliers', async (req, res) => {

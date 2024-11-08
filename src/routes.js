4bed5d3065c23cd1f9c2436ce74e5493391432f8
ItzChaBoiGhoza <cguy925@gmail.com>
2024-11-07 23:04:19 -0700

    adding a new route for items by id

--- src/routes.js
+++ src/routes.js
@@ -8,16 +8,25 @@ module.exports.register = (app, database) => {
 
     app.get('/api/items', async (req, res) => {
         let query;
-        query = database.query('SELECT * FROM items');
+        query = database.query('SELECT id, item_name FROM items');
 
         const records = await query;
 
         res.status(200).send(JSON.stringify(records)).end();
     });
 
+    app.get('/api/items/:id', async (req, res) => {
+        let query;
+        const params = req.params;
+
+        query = database.query('SELECT * FROM items WHERE id = ?', params);
+
+        res.status(200).send(JSON.stringify(records)).end();
+    })
+
     app.get('/api/suppliers', async (req, res) => {
         let query;
-        query = database.query('SELECT * FROM suppliers');
+        query = database.query('SELECT id, supplier_name FROM suppliers');
 
         const records = await query;
 
